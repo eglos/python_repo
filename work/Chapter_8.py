ce0450b016914d0d4f4fae886116d741de3856b9
temponic5496 <temponic5496@gmail.com>
2022-02-24 20:41:20 +0900

    20220224 3차

--- work/Chapter_8.py
+++ work/Chapter_8.py
@@ -94,18 +94,18 @@ print(t3)
 
 # 모든 키 얻기 : keys()
 signals = {'green':'go','yellow':'go faster','red':'smile for the camera'}
-set = list(signals.keys())
-print(set)
+set1 = list(signals.keys())
+print(set1)
 
 # 모든 값 얻기 : values()
 signals = {'green':'go','yellow':'go faster','red':'smile for the camera'}
-set = list(signals.values())
-print(set)
+set1 = list(signals.values())
+print(set1)
 
 # 모든 키-값 얻기 : items()
 signals = {'green':'go','yellow':'go faster','red':'smile for the camera'}
-set = list(signals.items())
-print(set)
+set1 = list(signals.items())
+print(set1)
 
 # 길이 얻기 : len()
 signals = {'green':'go','yellow':'go faster','red':'smile for the camera'}
@@ -196,3 +196,168 @@ signals['blue'] = 'confuse everyone'
 print(signals)
 
 print(original_signals)
+
+# 깊은 복사 : deepcopy()
+signals = {'green':'go','yellow':'go faster','red':['stop','smile']}
+signals_copy = signals.copy()
+print(signals)
+
+signals['red'][1] = 'sweat'
+print(signals)
+print(signals_copy) #얕은 복사가 발생
+
+# 깊은 복사 진행
+import copy
+signals = {'green':'go','yellow':'go faster','red':['stop','smile']}
+signals_copy = copy.deepcopy(signals)
+print(signals)
+print(signals_copy)
+
+signals['red'][1] = 'sweat'
+print(signals)
+print(signals_copy)
+
+#딕셔너리 비교
+a = {1:1,2:2,3:3}
+b = {3:3,1:1,2:2}
+print(a == b)
+
+# dict에는 비교연산자(<=, >=) 는 사용할 수 없다.
+
+# 파이썬은 원래 생성된 키/값의 순서에 상관없이 딕셔너리의 키/값을 하나씩 비교
+a = {1:[1,2], 2:[1], 3:[1]}
+b = {1:[1,1], 2:[1], 3:[1]}
+print(a == b)
+
+
+# 순회하기 : for 와 in
+accusation = {'room':'ballroom','weapon':'lead pipe','person':'Col.Mustard'}
+for card in accusation : # or, for card in accusation.keys() : 
+    print(card) # key 값들을 가져온다.
+    
+for value in accusation.values() :
+    print(value)
+    
+for item in accusation.items() :
+    print(item)
+    
+for card, contents in accusation.items() :
+    print('Card', card , 'has the contents',contents)
+    
+# 딕셔너리 컴프리헨션
+# {키_표현식 : 값_표현식 for 표현식 in 순회 가능한 객체}
+word = 'letters'
+letter_counts = {letter : word.count(letter) for letter in word}
+print(letter_counts)
+
+
+t = set(word)
+print(t)
+
+vowels = 'aeiou'
+word = 'onomatopoeia'
+vowel_counts = {letter : word.count(letter) for letter in set(word) if letter in vowels}
+print(vowel_counts)
+
+word = 'letters'
+letter_counts = {letter : word.count(letter) for letter in set(word)} ### ??
+print(letter_counts)
+
+
+# set : 값은 버리고 키만 남은 딕셔너리와 같다.
+#       딕셔너리와 마찬가지로 각 키는 유일해야 한다.
+
+empty_set = set()
+print(empty_set)
+
+even_numbers = {0,2,4,6,8}
+print(even_numbers)
+odd_numbers = {1,3,5,7,9}
+print(odd_numbers)
+
+# 변환하기 : set()
+print(set('letters')) # 문자가 두개씩 있어도 중복 제거하고 나타낸다.
+
+# 리스트를 셋으로 만들기
+tt1 = set(['Dasher','Dancer','Prancer','Mason-Dixon'])
+print(tt1)
+
+# 튜플을 셋으로 만들기 
+tt2 = set(('Ummagumma','Echoes','Atom Heart Mother'))
+print(tt2)
+
+# 딕셔너리에 set()을 사용하면 키만 사용
+tt3 = set({'apple':'red','orange':'orange','cherry':'red'})
+print(tt3)
+
+# 길이 얻기 : len()
+reindeer = set (['Dasher','Dancer','Prancer','Mason-Dixon'])
+print(len(reindeer))
+
+# 항목 추가하기 : add()
+s = set((1,2,3))
+print(s)
+s.add(4)
+print(s)
+
+# 항목 삭제하기 : remove()
+s = set((1,2,3))
+s.remove(3)
+print(s)
+
+# 순회하기 : for 와 in
+furniture = set(('sofa','ottoman','table'))
+for piece in furniture : 
+    print(piece)
+    
+    
+# 멤버십 테스트 : in
+drinks = {
+    'martini' : {'vodka','vermouth'},
+    'black russian' : {'vodka','kahlua'},
+    'white russian' : {'cream','kahlua','vodka'},
+    'manhattan' : {'rye','vermouth','bitters'},
+    'screwdriver' : {'orange juice','vodka'}
+}
+
+for name, contents in drinks.items() :
+    if 'vodka' in contents :
+        print(name)
+        
+for name, contents in drinks.items() :
+    if 'vodka' in contents and not contents &{'vermouth','cream'} :
+        print(name)
+        
+bruss = drinks['black russian']
+wruss = drinks['white russian'] 
+
+a = {1,2}
+b = {2,3}
+
+print(a&b)
+
+print(a.intersection(b)) #교집합
+
+print(bruss & wruss)
+
+print(a | b)
+print(a.union(b)) #합집합
+
+print (bruss | wruss )
+
+print ( a -  b) #차집합
+print(a.difference(b))
+
+print(bruss - wruss)
+
+print(wruss - bruss)
+
+print(a ^ b) # 대칭 차집합(여집합)
+print(a.symmetric_difference(b))
+
+print(bruss^wruss)
+
+print(a <= b) #부분집합
+print(a.issubset(b))
+
+print(bruss <= wruss) 
